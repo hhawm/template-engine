d1621ba415666b32d36d4b9db17a2eca313b63cf
henry hawm <henry.hawm@gmail.com>
2020-01-22 21:43:20 -0600

    js adjusted

--- app.js
+++ app.js
@@ -9,32 +9,144 @@ const Intern = require("./lib/intern");
 const Manager = require("./lib/manager");
 
 // Read or write files to promisify
-const writeFileAsync = util.promisify(fs.writeFileSync);
 const readFileAsync = util.promisify(fs.readFileSync);
+const writeFileAsync = util.promisify(fs.writeFileSync);
 
-const employeePrompt = [
+// Creates array of teamMembers
+let teamMembers = [];
+let teamName;
+
+// Team name and Questions for manager
+managerPrompt = [
     {
-        type: "list",
-        message: "Select an employee type",
-        name: "role",
-        choices: ["Engineer", "Intern"]
+        type: "input",
+        message: "Let's start a project. What is the team name?",
+        name: "teamname",
     },
     {
         type: "input",
-        message: "Employee Name",
-        name: "name",
-        validate: validate.name
+        message: "Who is the Manager of this project",
+        name: "managername"
+        // validate: validate.name
     },
     {
         type: "input",
-        message: "Employee ID",
-        name: "id",
-        validate: validate.int
+        message: "What is the Manager's ID?",
+        name: "managerid",
+        // validate: validate.int
     },
     {
         type: "input",
-        message: "Employee Email",
-        name: "email",
-        validate: validate.email
+        message: "What is the Manager's Email?",
+        name: "manageremail",
+        // validate: validate.email
+    },
+    {
+        type: "input",
+        message: "What is the Manager's Office Number?",
+        name: "officenumber",
+        // validate: validate.int
     }
 ];
+
+//// Questions for employees
+teamQuestions = [
+    {
+        type: "list",
+        message: "Select type of employee to add",
+        choices: ["Intern", "Engineer"],
+        name: "role",
+    },
+    {
+        type: "input",
+        message: "What is the employee's name?",
+        name: "employeename",
+        // validate: validate.name
+    },
+    {
+        type: "input",
+        message: "What is the employee's ID?",
+        name: "employeeid",
+        // validate: validate.int
+    },
+    {
+        type: "input",
+        message: "What is the employee's Email?",
+        name: "employeeemail",
+        // validate: validate.email
+    },
+    {
+        type: "input",
+        message: "Engineer's GitHub user name?",
+        when: (userResponse) => userResponse.role === "Engineer",
+        name: "github",
+    },
+    {
+        type: "input",
+        message: "Intern's school name?",
+        when: (userResponse) => userResponse.role === "Intern",
+        name: "school",
+    },
+    {
+        type: "confirm",
+        message: "Would you like to add another employee to the team: " + teamname + "?",
+        name: "additonalmember"
+    },
+];
+
+
+
+//functions to help the async init
+
+function managerData() {
+    inquirer.prompt(managerQuestions)
+        .then(managerResponse => {
+            teamName = managerResponse.teamname;
+            const managerName = managerResponse.managername;
+            const managerId = managerResponse.managerid;
+            const managerEmail = managerResponse.manageremail;
+            const officeNumber = managerResponse.officeNumber;
+
+            //create a new manager and add them to teamMember array
+            const manager = new Manager(managerName, managerId, managerEmail, officeNumber);
+            teamMembers.push(manager);
+            teamData();
+        })
+}
+
+function teamData() {
+    inquirer.prompt(teamQuestions)
+        .then(userResponse => {
+            const role = userResponse.role;
+            const employeeName = userResponse.employeename;
+            const employeeId = userResponse.employeeid;
+            const employeeEmail = userResponse.employeeemail;
+            const github = userResponse.github;
+            const school = userResponse.school;
+            const additonalMember = userResponse.additonalmember;
+
+            //create either new engineer or intern
+            if (role === "Engineer") {
+                const engineer = new Engineer(employeeName, employeeId, employeeEmail, github);
+                teamMembers.push(engineer);
+            } else if (role === "Intern") {
+                const intern = new Intern(employeeName, employeeId, employeeEmail, school);
+                teamMembers.push(intern);
+            }
+
+            //create a statment that lets the function run for as many members the team needs
+            if (additonalMember === true) {
+                teamData();
+            } else {
+                //render the team
+
+                // console.log(teamName);
+                // console.log(teamMembers);
+            }
+        })
+}
+
+console.log(teamMembers);
+
+
+managerData();
